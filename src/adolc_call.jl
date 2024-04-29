4d1e7cbdc902774dffb9fd610aad5cb0435cbae2
shtadinada <100642632+shtadinada@users.noreply.github.com>
2024-04-29 13:19:16 +0200

    Update adolc_call.jl

--- src/adolc_call.jl
+++ src/adolc_call.jl
@@ -1,28 +1,35 @@
 using ADOLC
-using ADOLC.TbadoubleModule
-using ADOLC.array_types
 
 function call_adolc(x, f)
 # call the abs_normal driver
 n = length(x)
 y = Vector{Float64}(undef, 1)
 m = length(y)
-a = [Adouble{TbAlloc}() for _ in 1:length(x)]
-b = [Adouble{TbAlloc}() for _ in 1:length(y)]
-
-tape_num = 0
-trace_on(tape_num, 1)
-a << x
-b[1] = f(a)
-b >> y
-trace_off(0)
-
-m = length(b)
-
-abs_normal_problem = AbsNormalProblem{Float64}(tape_num, m, n, x, y)
 
 
-abs_normal!(abs_normal_problem, tape_num)
+# Initialize the AbsNormalForm object
+ abs_normal_form = ADOLC.AbsNormalForm()
 
-    return abs_normal_problem
+# Calculate the absolute normal form derivative
+ derivative!(abs_normal_form, f, m, n, x, :abs_normal)
+
+
+    return abs_normal_form
+end
+
+function call_adolc_reuse(x, f)
+# call the abs_normal driver
+n = length(x)
+y = Vector{Float64}(undef, 1)
+m = length(y)
+
+
+# Initialize the AbsNormalForm object
+ abs_normal_form = ADOLC.AbsNormalForm()
+
+# Calculate the absolute normal form derivative
+ derivative!(abs_normal_form, f, m, n, x, :abs_normal, tape_id = abs_normal_form.tape_id, reuse_tape =true)
+
+
+    return abs_normal_form
 end
