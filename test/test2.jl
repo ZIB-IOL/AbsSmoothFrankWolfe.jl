a5f19d52ecbb21ea117eef78b09d3b99d4c7d9c3
shtadinada <100642632+shtadinada@users.noreply.github.com>
2024-05-06 16:54:45 +0200

    Update test2.jl

--- test/test2.jl
+++ test/test2.jl
@@ -1,24 +1,29 @@
-using ADOLC, ADOLC.array_types
-
-function demo()
- function f(x)
-        return (max(-x[1]-x[2], -x[1]-x[2]+x[1]^2+x[2]^2-1) + max(-x[2]-x[3], -x[2]-x[3]+x[2]^2+x[3]^2-1))
-    end 
+function demo_abs_normal()
+    # Define the Mifflin 2 function
+    function f(x)
+        return -x[1] + 2*(x[1]^2 + x[2]^2 - 1) + 1.75*abs(x[1]^2 + x[2]^2 - 1)
+    end
     
-    x = [-1.5, -1.5, -1.5]
+    # Define the derivative evaluation point x
+    x = [-1.0, -1.0]
+    
+    # Initialize the AbsNormalForm object
+    abs_normal_form = ADOLC.init_abs_normal_form(f, 1, 2, x, tape_id=1)
+    
+    # Calculate the absolute normal form derivative
+    derivative!(abs_normal_form, f, 1, 2, x, :abs_normal, tape_id=abs_normal_form.tape_id, reuse_tape=true)
+    
+    println("AbsNormalForm at $x: ", abs_normal_form)
+    
+    # some computations
+    # ....
+    
+    # new evaluation point
+    x = [-0.5, 1.0]
+    derivative!(abs_normal_form, f, 1, 2, x, :abs_normal, tape_id=abs_normal_form.tape_id, reuse_tape=true)
 
-    abs_normal_problem = ADOLC.AbsNormalForm()
-    derivative!(abs_normal_problem, f, 1, 3, x, :abs_normal)
-    y = f(x)
-
-    @show abs_normal_problem.y[1]
-
-    x = [-0.5, -0.5, -0.5]
-    # reuse abs_normal_problem with same id and without retaping
-    derivative!(abs_normal_problem, f, 1, 3, x, :abs_normal, tape_id=abs_normal_problem.tape_id, reuse_tape=true)
-    y = f(x)
-
-     @show abs_normal_problem.Y[1, 1]
+    println("AbsNormalForm at $x: ", abs_normal_form)
 end
- demo()
+demo_abs_normal()
+
 
