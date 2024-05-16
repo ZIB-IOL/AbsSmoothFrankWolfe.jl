62930e8c360e8995cb6893084a076623aa7762cf
shtadinada <100642632+shtadinada@users.noreply.github.com>
2024-05-16 18:10:10 +0200

    Create example_l.jl

---
+++ examples/example_l.jl
@@ -0,0 +1,85 @@
+using FrankWolfe
+using SparseArrays
+using LinearAlgebra
+using JuMP
+using HiGHS
+using ADOLC
+
+
+import MathOptInterface
+const MOI = MathOptInterface
+
+include("../src/aasm.jl")
+include("../src/as_frank_wolfe.jl")
+include("../src/abs_linear.jl")
+include("../src/abs_lmo.jl")
+
+# Chained Mifflin 2  
+ function f(x)
+     n = length(x)
+     l_1 = [-x[i]+2*(x[i]^2+x[i+1]^2-1)+1.75*abs(x[i]^2+x[i+1]^2-1) for i in 1:n-1]
+     return sum(l_1[i] for i in 1:n-1)
+ end
+ 
+n = 1000
+
+# evaluation point x_base
+x_base = ones(n)
+n = length(x_base)
+
+lb_x = [-10 for in in x_base] 
+ub_x = [10 for in in x_base]
+
+# call the abs-linear form of f
+abs_normal_form = abs_linear(x_base,f)
+
+alf_a = abs_normal_form.Y 
+alf_b = reshape(abs_normal_form.J, size(abs_normal_form.J)[2], 1)
+z = abs_normal_form.z  
+s = abs_normal_form.num_switches
+
+sigma_z = signature_vec(s,z)
+
+# gradient formula in terms of abs-linearization
+function grad!(storage, x)
+    c = vcat(alf_a', alf_b.* sigma_z)
+    @. storage = c
+end
+
+# set bounds
+o = Model(HiGHS.Optimizer)
+MOI.set(o, MOI.Silent(), true)
+@variable(o, lb_x[i] <= x[i=1:n] <= ub_x[i])
+
+# initialise dual gap 
+dualgap_asfw = Inf
+
+# abs-smooth lmo
+lmo_as = AbsSmoothLMO(o, x_base, f, n, s, lb_x, ub_x, dualgap_asfw)
+
+# define termination criteria
+
+# In case we want to stop the frank_wolfe algorithm prematurely after a certain condition is met,
+# we can return a boolean stop criterion `false`.
+# Here, we will implement a callback that terminates the algorithm if ASFW Dual gap < eps.
+function make_termination_callback(state)
+ return function callback(state,args...)
+  return norm(state.lmo.dualgap_asfw) > 1e-3
+ end
+end
+
+callback = make_termination_callback(FrankWolfe.CallbackState)
+
+# call abs-smooth-frank-wolfe
+x, v, primal, dual_gap, traj_data = as_frank_wolfe(
+    f, 
+    grad!, 
+    lmo_as, 
+    x_base;
+    gradient = ones(n+s),
+    line_search = FrankWolfe.FixedStep(1.0),
+    callback=callback,
+    verbose=true,
+    max_iteration=1e7
+)
+
