047fc7fcaba5b93b0f39854125fd52a543058fe2
shtadinada <100642632+shtadinada@users.noreply.github.com>
2024-05-16 17:09:50 +0200

    Update abs_lmo.jl

--- src/abs_lmo.jl
+++ src/abs_lmo.jl
@@ -12,11 +12,12 @@ mutable struct AbsSmoothLMO <: FrankWolfe.LinearMinimizationOracle
     s
     lb_x 
     ub_x
+    dualgap_asfw
     fw_iteration_counter::Int # for FW
     iteration_counter::Int # for inner iterations
 end
 
-AbsSmoothLMO(o, x_base, f, n, s, lb_x, ub_x) = AbsSmoothLMO(o, x_base, f, n, s, lb_x, ub_x, 0, 0) 
+AbsSmoothLMO(o, x_base, f, n, s, lb_x, ub_x, dualgap_asfw) = AbsSmoothLMO(o, x_base, f, n, s, lb_x, ub_x, dualgap_asfw, 0, 0) 
 
 # abs-smooth custom extreme point -- AASM
 
@@ -30,7 +31,11 @@ function FrankWolfe.compute_extreme_point(lmo::AbsSmoothLMO, direction; kwargs..
     inner_iteration = lmo.iteration_counter
     t = lmo.fw_iteration_counter
     alpha = 2 /(t+2)    
-    x_delta, fabs, _, _, iter = aasm(x_base, alpha, f, n, ub_x, lb_x, inner_iteration)
+    x_delta, gap, _, _, iter = aasm(x_base, alpha, f, n, ub_x, lb_x, inner_iteration)
+    
+    # update abs-smooth dual gap
+    lmo.dualgap_asfw = gap
+    #@show lmo.dualgap_asfw
     
     # update inner_iteration
      lmo.fw_iteration_counter += 1
