03f530b4f405cfbfd2558942bfd974f1b4eae627
shtadinada <100642632+shtadinada@users.noreply.github.com>
2024-05-16 17:19:13 +0200

    Update abs_linear.jl

--- src/abs_linear.jl
+++ src/abs_linear.jl
@@ -5,28 +5,13 @@ f: abs-smooth func
 """
 
 function abs_linear(x,f)
+    n = length(x)
 
     # Initialize the AbsNormalForm object
-    abs_normal_form = ADOLC.init_abs_normal_form(f, 1, 2, x, tape_id=1)
+    abs_normal_form = ADOLC.init_abs_normal_form(f, 1, n, x, tape_id=1)
     
     # Calculate the absolute normal form derivative
-    derivative!(abs_normal_form, f, 1, 2, x, :abs_normal, tape_id=abs_normal_form.tape_id, reuse_tape=true)
+    derivative!(abs_normal_form, f, 1, n, x, :abs_normal, tape_id=abs_normal_form.tape_id, reuse_tape=true)
        
-    return abs_normal_form
-end
-
-
-function call_adolc_(x, f)
-
-n = length(x)
-y = Vector{Float64}(undef, 1)
-m = length(y)
-
-# Initialize the AbsNormalForm object
-abs_normal_form = ADOLC.init_abs_normal_form(f, m, n, x, tape_id=1)
-    
-# Calculate the absolute normal form derivative
-derivative!(abs_normal_form, f, m, n, x, :abs_normal)
-  
     return abs_normal_form
 end
