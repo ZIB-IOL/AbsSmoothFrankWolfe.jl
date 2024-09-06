53af011db9eba94f39aa68323bed29cc426446d0
shtadinada <tsriharshitha@gmail.com>
2024-09-06 12:52:59 +0200

    ci and make

--- docs/make.jl
+++ docs/make.jl
@@ -2,20 +2,15 @@ using Documenter
 using AbsSmoothFW
 
 push!(LOAD_PATH,"../src/")
-makedocs(
-    sitename = "AbsSmoothFW.jl Documentation",
+makedocs(;
+    sitename = "AbsSmoothFW.jl",
+    modules = [AbsSmoothFW],
+    format = Documenter.HTML(),
     pages = [
-            "Index" => "index.md",
-    format = Documenter.HTML(prettyurls = false)
- 
+            "Home" => "index.md",
+   ],
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-deploydocs(
-    repo = "github.com/shtadinada/AbsSmoothFW.jl.git",
-    devbranch = "main"
-)
+deploydocs(; repo = "github.com/shtadinada/AbsSmoothFW.jl.git", devbranch = "main")
 
 
