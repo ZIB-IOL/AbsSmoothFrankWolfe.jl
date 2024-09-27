7af5a1efac9f520d4d9034721e037c746ade68e8
shtadinada <tsriharshitha@gmail.com>
2024-09-28 01:37:42 +0200

    update make

--- docs/make.jl
+++ docs/make.jl
@@ -31,5 +31,5 @@ makedocs(;
    ],
 )
 
-deploydocs(; repo = "github.com/ZIB-IOL/AbsSmoothFrankWolfe.jl", devbranch="main", push_preview=true)
+deploydocs(; repo = "github.com/ZIB-IOL/AbsSmoothFrankWolfe.jl.git", devbranch="main", push_preview=true)
 
