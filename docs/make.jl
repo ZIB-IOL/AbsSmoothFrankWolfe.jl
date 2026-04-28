0f03164c4cb5aec5e95f8120b22283ce52dfedd1
Sri H Tadinada <100642632+shtadinada@users.noreply.github.com>
2026-04-28 12:36:41 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,25 +1,6 @@
 using Documenter
 using AbsSmoothFrankWolfe
 
-generated_path = joinpath(@__DIR__, "src")
-base_url = "https://github.com/ZIB-IOL/AbsSmoothFrankWolfe.jl/tree/main/"
-isdir(generated_path) || mkdir(generated_path)
-
-open(joinpath(generated_path, "index.md"), "w") do io
-    # Point to source license file
-    println(
-        io,
-        """
-        ```@meta
-        EditURL = "$(base_url)README.md"
-        ```
-        """,
-    )
-    # Write the contents out below the meta block
-    for line in eachline(joinpath(dirname(@__DIR__), "README.md"))
-        println(io, line)
-    end
-end
 
 makedocs(;
     sitename = "AbsSmoothFrankWolfe.jl",
